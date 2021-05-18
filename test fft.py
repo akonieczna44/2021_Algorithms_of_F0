aeeca905527e7dd453d75c5da6fc76f9c3034871
bpanther44 <konieczko614@interia.pl>
2021-05-19 01:12:12 +0200

    update test fft

--- test fft.py
+++ test fft.py
@@ -1,3 +1,50 @@
 # test fft czemu to nie działa
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 1000
+A = 5
+fx = 10
+fp = 1000
+
+# okres próbkowania
+dt = 1/fp
+
+xx = 0
+t = []
+for i in range(N):
+    t.append(xx*dt)
+    xx = xx+1
+
+#print(t)
+
+y = A * np.sin(2*3*fx*t)
+print(len(y))
+
+# y fft
+X = np.fft.fft(y)
+df = 1/(N*dt)
+
+xxx = 0
+y_fft = []
+for i in range(N):
+    y_fft.append(xxx*df)
+    xxx = xxx+1
+
+plt.plot(y)
+plt.show()
+#plt.plot(X)
+
+z = np.corrcoef(y)
+#print(len(z))
+print(z)
+
+
+
+
+plt.show()
+
+
+
 
 
