b0372043da470aeb7a6f52a99871830c650c1da8
bpanther44 <konieczko614@interia.pl>
2021-05-23 19:54:21 +0200

    autokorelacja new

--- f0_autocorellation.py
+++ f0_autocorellation.py
@@ -1,5 +1,69 @@
 # autokorelacja
-
 # wychodzi w miarę w porządku, ale trzeba to przepisać ładnie i dopisać tutaj
-
 # przy vib1 wychodzi wszędzie inaczej lub tylko autokorelacja dobrze
+
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa.display
+from tkinter import filedialog
+from scipy.fftpack import fft
+from scipy.signal import blackmanharris, correlate
+from time import time
+
+print('start')
+
+
+def parabolic(f, x):
+    """Quadratic interpolation for estimating the true position of an
+    inter-sample maximum when nearby samples are known.
+    f is a vector and x is an index for that vector.
+    Returns (vx, vy), the coordinates of the vertex of a parabola that goes
+    through point x and its two neighbors.
+    Example:
+    Defining a vector f with a local maximum at index 3 (= 6), find local
+    maximum if points 2, 3, and 4 actually defined a parabola.
+    In [3]: f = [2, 3, 1, 6, 4, 2, 3, 1]
+    In [4]: parabolic(f, argmax(f))
+    Out[4]: (3.2142857142857144, 6.1607142857142856)
+    """
+    xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
+    yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
+    return (xv, yv)
+
+
+def freq_from_autocorr(sig, fs):
+    """
+    Estimate frequency using autocorrelation
+    """
+    # Calculate autocorrelation and throw away the negative lags
+    corr = correlate(sig, sig, mode='full')
+    corr = corr[len(corr)//2:]
+
+    # Find the first low point
+    d = np.diff(corr)
+    start = np.nonzero(d > 0)[0][0]
+
+    # Find the next peak after the low point (other than 0 lag).  This bit is
+    # not reliable for long signals, due to the desired peak occurring between
+    # samples, and other peaks appearing higher.
+    # Should use a weighting function to de-emphasize the peaks at longer lags.
+    peak = np.argmax(corr[start:]) + start
+    print('peak is ',peak)
+    px, py = parabolic(corr, peak)
+
+    return fs / px
+
+
+y, fs = librosa.load(filedialog.askopenfilename(title="Wybierz plik", filetypes = (("wav mono files",".wav"), ("all files", "*.*"))))
+
+
+print('Peak ', end=' ')
+start_time = time()
+print('Autokorelacja F0 = %f Hz' % round(freq_from_autocorr(y, fs),4))
+print('Time elapsed: %.3f s\n' % (time() - start_time))
+
+# to do - autok.
+plt.plot(y)
+plt.show()
+
